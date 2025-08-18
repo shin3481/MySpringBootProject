49969c0fb982b5c65ec3e201e3a7ee0e49348783
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-08-18 21:15:39 +0900

    SpringBoot Controller&templates

--- src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -5,39 +5,67 @@ import com.rookies4.myspringboot.exception.BusinessException;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequiredArgsConstructor //롬북으로 생성자를 자동으로 시켜줌, (Constructor Injection 대신에 사용)
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserRestController {
-    private final UserRepository userRepository; //final을 적으면 변수를 무조권 초기화시킴
+    private final UserRepository userRepository;
 
-//    //Constructor Injection
+    //Constructor Injection
 //    public UserRestController(UserRepository userRepository) {
 //        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
 //        this.userRepository = userRepository;
 //    }
+
     //등록
     @PostMapping
     public UserEntity create(@RequestBody UserEntity user){
         return userRepository.save(user);
-
     }
-    //전체목록조회
+
+    //전체목록 조회
     @GetMapping
-    public List<UserEntity> getUsers(){
+    public List<UserEntity> getUsers() {
         return userRepository.findAll();
     }
     //ID로 조회
-    @GetMapping("/{id}")//api/users/1
-    public UserEntity getUser(@PathVariable Long id){ //@PathVariable  /1, /2같이 어떤 id를 가져오는지
-        Optional<UserEntity> optionalUser = userRepository.findById(id);
-        //orElseThrow(Supplier) Supplier의 추상메서드 T get()
-        UserEntity existUser = optionalUser.orElseThrow(()-> new BusinessException("User Not Found", HttpStatus.NOT_FOUND)); //실패하면 예외던지기(404)
+    @GetMapping("/{id}")
+    public UserEntity getUser(@PathVariable Long id){
+        UserEntity existUser = getExistUser(id);
         return existUser;
     }
+    //Email로 조회하고, 수정
+    @PatchMapping("/{email}/")
+    public UserEntity updateUser(@PathVariable String email, @RequestBody UserEntity userDetail){
+        UserEntity existUser = userRepository.findByEmail(email) //Optional<UserEntity>
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        //name 변경
+        existUser.setName(userDetail.getName());
+        //DB에 저장
+        UserEntity updateUser = userRepository.save(existUser);
+        return updateUser;
+    }
+    //삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+        UserEntity existUser = getExistUser(id);
+        //DB에 삭제요청
+        userRepository.delete(existUser);
+        return ResponseEntity.ok("User가 삭제 되었습니다.");
+    }
+
+    private UserEntity getExistUser(Long id) {
+        Optional<UserEntity> optionalUser = userRepository.findById(id);
+        //orElseThrow(Supplier) Supplier의 추상메서드 T get()
+        UserEntity existUser = optionalUser
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        return existUser;
+    }
+
 }
--- src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
+++ src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
@@ -1,6 +1,9 @@
 package com.rookies4.myspringboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,10 +21,13 @@ public class UserEntity {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 
+@NotEmpty(message = "name은 필수 입력항목 입니다.")
 @Column(nullable = false)
 private String name;
 
+@NotBlank(message = "email은 필수 입력항목 입니다.")
 @Column(unique = true, nullable = false)
+@Email
 private String email;
 
 @Column(nullable = false, updatable = false)
