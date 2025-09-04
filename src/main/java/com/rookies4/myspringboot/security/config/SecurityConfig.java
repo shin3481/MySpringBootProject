ce48fa687dd9b8f802d010c4206ad1d0ee0b9381
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-09-04 16:00:57 +0900

    SecurityConfig authentication bean add

--- src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
+++ src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.rookies4.myspringboot.security.config;
 import com.rookies4.myspringboot.security.service.UserInfoUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -46,6 +48,12 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
