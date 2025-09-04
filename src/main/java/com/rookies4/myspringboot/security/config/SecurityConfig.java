31388c8b1dff2e9903345cd3b9376a97d3094eab
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-09-04 14:42:01 +0900

    UserInfoRepository add

--- src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
+++ src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
